f512207594388870b8197c57efdd4ed676c0917a
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2024-01-30 16:46:01 -0500

    Update build-deb.sh

--- build-deb.sh
+++ build-deb.sh
@@ -11,7 +11,7 @@ wget http://deb.debian.org/debian/pool/main/c/cheese/cheese_44.1.orig.tar.xz
 wget http://deb.debian.org/debian/pool/main/c/cheese/cheese_44.1-1.debian.tar.xz
 
 xz -cd cheese_44.1.orig.tar.xz | tar xvf -
-cd cheese*
+cd cheese-44.1
 
 xz -cd ../cheese_44.1-1.debian.tar.xz | tar xvf -
 
