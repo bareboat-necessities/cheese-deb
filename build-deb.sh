b469e17e73c165d33fcc8602169fdd2634f1c921
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2024-01-30 16:34:30 -0500

    Update build-deb.sh

--- build-deb.sh
+++ build-deb.sh
@@ -5,4 +5,7 @@ sudo apt-get -y install meson cmake make git \
 
 sudo apt-get -y install build-essential git cmake flex bison 
 
+wget http://deb.debian.org/debian/pool/main/c/cheese/cheese_44.1.orig.tar.xz
+http://deb.debian.org/debian/pool/main/a/avahi/avahi_0.8-10.debian.tar.xz
+
 dpkg-buildpackage -b -d -uc -us
