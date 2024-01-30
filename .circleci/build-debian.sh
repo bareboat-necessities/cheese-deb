3ae707c11bdbf3bc4fc1a2ef4d0217b6b07fced6
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2024-01-30 17:59:11 -0500

    Update build-debian.sh

--- .circleci/build-debian.sh
+++ .circleci/build-debian.sh
@@ -21,8 +21,6 @@ if [ "$EMU" = "on" ]; then
   fi
 fi
 
-cp /etc/apt/sources.list /etc/apt/sources.list~; sed -Ei 's/^# deb-src /deb-src /' /etc/apt/sources.list; apt-get update
-
 WORK_DIR=$(pwd):/ci-source
 
 docker run --privileged --cap-add=ALL --security-opt="seccomp=unconfined" -d -ti -e "container=docker"  -v $WORK_DIR:rw $DOCKER_IMAGE /bin/bash
