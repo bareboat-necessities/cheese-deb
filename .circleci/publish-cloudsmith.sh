18fcfccc43f820bd6ed0889a5de484b3fa967f59
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2024-01-30 18:01:25 -0500

    Create publish-cloudsmith.sh

---
+++ .circleci/publish-cloudsmith.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+EXT=$1
+REPO=$2
+DISTRO=$3
+
+for pkg_file in dist/*.$EXT; do
+  echo publishing "cloudsmith push $EXT $REPO/$DISTRO $pkg_file"
+  cloudsmith push $EXT $REPO/$DISTRO $pkg_file
+  RESULT=$?
+  if [ $RESULT -eq 144 ]; then
+     echo "skipping already deployed $pkg_file"
+  fi
+done
