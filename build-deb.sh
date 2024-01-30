3fd829a8531db2c047097bb177d94ea17bd7f4fe
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2024-01-30 16:39:19 -0500

    Update build-deb.sh

--- build-deb.sh
+++ build-deb.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+mkdir cheese-tmp; cd cheese-tmp
+
 sudo apt-get -y install meson cmake make git \
  dpkg-dev debhelper devscripts equivs pkg-config
 
@@ -11,3 +13,6 @@ wget http://deb.debian.org/debian/pool/main/c/cheese/cheese_44.1-1.debian.tar.xz
 xz -cd cheese_44.1-1.debian.tar.xz | tar xvf -
 
 dpkg-buildpackage -b -d -uc -us
+
+cd ..
+
