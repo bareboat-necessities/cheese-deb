d4ca3895b18e2785eb8b353ca13a0b150d6ff8af
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2024-01-30 16:58:31 -0500

    Update build-deb.sh

--- build-deb.sh
+++ build-deb.sh
@@ -5,7 +5,7 @@ mkdir cheese-tmp; cd cheese-tmp
 sudo apt-get -y install meson cmake make git \
  dpkg-dev debhelper devscripts equivs pkg-config
 
-sudo apt-get -y install build-essential git cmake flex bison 
+sudo apt-get -y install build-essential git cmake flex bison valac
 
 wget http://deb.debian.org/debian/pool/main/c/cheese/cheese_44.1.orig.tar.xz
 wget http://deb.debian.org/debian/pool/main/c/cheese/cheese_44.1-1.debian.tar.xz
