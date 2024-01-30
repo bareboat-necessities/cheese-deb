4a47fa239a0800b0754a5ef937473268a395b7fd
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2024-01-30 16:40:14 -0500

    Update build-deb.sh

--- build-deb.sh
+++ build-deb.sh
@@ -10,9 +10,14 @@ sudo apt-get -y install build-essential git cmake flex bison
 wget http://deb.debian.org/debian/pool/main/c/cheese/cheese_44.1.orig.tar.xz
 wget http://deb.debian.org/debian/pool/main/c/cheese/cheese_44.1-1.debian.tar.xz
 
-xz -cd cheese_44.1-1.debian.tar.xz | tar xvf -
+xz -cd cheese_44.1.orig.tar.xz | tar xvf -
+cd cheese*
+
+xz -cd ../cheese_44.1-1.debian.tar.xz | tar xvf -
 
 dpkg-buildpackage -b -d -uc -us
+
+cd ..
 
 cd ..
 
